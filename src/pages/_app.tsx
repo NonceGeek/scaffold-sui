432938c51b7d1a4b6e223a14469d0d4c3be29877
v1xingyue <qixingyue@126.com>
2023-02-13 18:16:09 +0800

    module 模块跳转到浏览器

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,7 +14,7 @@ function WalletSelector({ Component, pageProps }: AppProps) {
     <WalletProvider defaultWallets={AllDefaultWallets}>
       <div className="px-8 rootimage min-h-screen">
         <NavBar />
-        <div>
+        <div className="p-8">
           <Component {...pageProps} className="bg-base-300" />
         </div>
       </div>
--- src/pages/contract.tsx
+++ src/pages/contract.tsx
@@ -1,6 +1,6 @@
 import { useWallet } from "@suiet/wallet-kit";
 import { useEffect, useState } from "react";
-import { SUI_PACKAGE, SUI_MODULE } from "../config/constants";
+import { SUI_PACKAGE, SUI_MODULE, NETWORK } from "../config/constants";
 import { JsonRpcProvider } from '@mysten/sui.js';
 
 
@@ -49,15 +49,15 @@ export default function Contract() {
     }
 
     return (
-        <>
-            <div className="card lg:card-side bg-base-100 shadow-xl mt-5">
-                <div className="card-body">
-                    <h2 className="card-title">
-                        Sui Test Package Module: {SUI_PACKAGE}
-                    </h2>
+        <div>
+            <div className="alert alert-info shadow-lg">
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-current flex-shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                    <span>
+                        Sui Test Package Module: <a target={"_blank"} href={"https://explorer.sui.io/object/" + SUI_PACKAGE + "?network=" + NETWORK}>{SUI_PACKAGE}</a>
+                    </span>
                 </div>
             </div>
-
 
             <div className="card lg:card-side bg-base-100 shadow-xl mt-5">
                 <div className="card-body">
@@ -96,6 +96,6 @@ export default function Contract() {
                 </div>
             </div>
 
-        </>
+        </div>
     );
 }
