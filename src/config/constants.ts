3255016e629e331a008490a0291800a636bc30c3
v1xingyue <qixingyue@126.com>
2023-02-17 10:41:27 +0800

    去除无用代码

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,6 +1,3 @@
-export const NFT_STORAGE_KEY = process.env.NEXT_PUBLIC_NFT_STORAGE_KEY!;
-export const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_KEY!;
-export const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 export const SUI_PACKAGE = process.env.NEXT_PUBLIC_DAPP_PACKAGE!; // changed here.
 export const SUI_MODULE = process.env.NEXT_PUBLIC_DAPP_MODULE!; // changed here.
 export const NETWORK = process.env.NEXT_PUBLIC_SUI_NETWORK!;
