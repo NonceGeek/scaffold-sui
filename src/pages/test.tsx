5743fa844c4665dffc10f1858177758ed9def0c1
v1xingyue <qixingyue@126.com>
2023-05-30 13:03:07 +0800

    test split coin

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,26 +1,47 @@
 import { useWallet } from "@suiet/wallet-kit";
-import {  TransactionBlock } from '@mysten/sui.js';
+import {  JsonRpcProvider, TransactionBlock, devnetConnection } from '@mysten/sui.js';
+import { useEffect, useState } from "react";
 
 const Test = ()=>{
-    const { address} = useWallet();
+    const { address,signAndExecuteTransactionBlock} = useWallet();
+    const [result,updateResult] = useState({});
+    const provider = new JsonRpcProvider(devnetConnection);
+
+
+    useEffect(()=>{
+        if(address){
+            (async()=>{
+                const coins = await provider.getOwnedObjects({
+                    owner:address
+                })
+                console.log(coins);
+            })();
+        }
+    },[address])
+    
+
+
     const testHandle = async()=>{
-        console.log(address);
-        console.log("=====....");
         const tx = new TransactionBlock();
-        console.log("=====");
-        const [coins] = tx.splitCoins(tx.gas,[tx.pure(10000)]);
-        console.log("=====.####...");
-        tx.transferObjects([coins], tx.pure(address));
-        console.log("=====.###OOOO#...");
+        const coins = tx.splitCoins(tx.gas,[tx.pure(1)]);
+        tx.transferObjects([coins[0]], tx.pure(address));
+        const result = await  signAndExecuteTransactionBlock({
+            transactionBlock:tx
+        });
+        updateResult(result);
     }
 
     return (
         <>
             <div>
 
+                <p>{address}</p>
                 <div className="mt-3 mb-3">
                     <button onClick={testHandle} className="btn btn-primary">Click for test!!!</button>
                 </div>
+                <pre>
+                    {JSON.stringify(result)}
+                </pre>
                 
             </div>
         </>
